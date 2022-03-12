23ed2776c21ef70d284ddd3aca52ddc5006b2390
Thanapon <thanapon160@hotmail.com>
2022-03-12 22:44:35 +0700

    learn create more method

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res
+} from '@nestjs/common'
+import { response } from 'express'
 
 @Controller('coffees')
 export class coffeesController {
@@ -6,4 +16,26 @@ export class coffeesController {
   findAll() {
     return 'This action return all the coffees'
   }
+  // @Get('flavors')
+  // findAll(@Res() response) {
+  //   response.status(200).send('This action return all the coffees')
+  // }
+
+  @Get(':id/:page')
+  findOne(@Param('id') id: string, @Param('page') page: string) {
+    console.log(id, page)
+    return `This action returns #${id} coffee by ${page}`
+  }
+
+  @Post()
+  create(@Body() body: Record<string, string>) {
+    console.log(body)
+    return body
+  }
+  // @Post()
+  // @HttpCode(HttpStatus.CREATED)
+  // create(@Body() body: Record<string, string>) {
+  //   console.log(body)
+  //   return body
+  // }
 }
