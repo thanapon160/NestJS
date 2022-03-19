a517e25fd4624b91b3d62bb306ab8adc577d0ac8
Thanapon <thanapon160@hotmail.com>
2022-03-19 16:01:49 +0700

    learn update delete query

--- note.md
+++ note.md
@@ -1,1 +1,7 @@
-##
+### Decorator
+## Query
+```bash
+# Query param after '?"
+(@Query() val)
+localhost:3000/coffees/flavors?val=xxxx
+```
--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,20 +1,25 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
+  Query,
   Res
 } from '@nestjs/common'
 import { response } from 'express'
 
 @Controller('coffees')
 export class coffeesController {
+  // #Method Get
   @Get('flavors')
-  findAll() {
-    return 'This action return all the coffees'
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery
+    return `This action return all the coffees. Limit: ${limit}, offset: ${offset}`
   }
   // @Get('flavors')
   // findAll(@Res() response) {
@@ -27,6 +32,7 @@ export class coffeesController {
     return `This action returns #${id} coffee by ${page}`
   }
 
+  // #Method Post
   @Post()
   create(@Body() body: Record<string, string>) {
     console.log(body)
@@ -38,4 +44,16 @@ export class coffeesController {
   //   console.log(body)
   //   return body
   // }
+
+  // #Method Update
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`
+  }
+
+  // #Method Delete
+  @Delete(':id')
+  remove(@Param('id') id: string, @Body() body) {
+    return `This action removes #${id} coffee`
+  }
 }
